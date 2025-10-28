064482f9729099706ac0c9f811b5644ff7aff3c3
吴玉海 <13531762262@163.com>
2025-10-28 15:54:08 +0800

    Auto commit - 2025/10/28 周二 15:54:08.59

--- 资源服务器102.bat
+++ 资源服务器102.bat
@@ -5,30 +5,39 @@
 :: wfyl 2025/10/28 13:54:30
 :: ==========================================================
 
-echo Auto git commit script started
+echo ----------------------------------------------------------
+echo GO!GO!GO!... started
+echo ----------------------------------------------------------
 set target=w:\java\SoftwareGraduationDesign\ACNiuOJ\wfyl.github.io
 set interval=50
 
+echo ----------------------------------------------------------
 :loop
-echo Checking for changes...
+echo Checking if new Info
 
 if not exist "%target%" (
-    echo Error: Target directory not found
+    echo Error: ifdir!!NO!! dir not found
     ping -n 5 127.0.0.1 > nul
     goto loop
 )
 
+echo ----------------------------------------------------------
 cd /d "%target%"
 
 if not exist ".git" (
-    echo Initializing git repository...
+    echo into Git home... Initializing git repository...
     git init
 )
 
+echo ----------------------------------------------------------
 git add .
 git commit -m "Auto commit - %date% %time%"
+echo Going Git Home ! GO!GO!GO! HeiKaFeiPingWeiYouDuoNong ......
 git push origin HEAD
 
-echo Waiting %interval% seconds...
+echo ----------------------------------------------------------
+
+echo ----------------------------------------------------------
+echo Waiting %interval% s time to run Next GO!GO!GO!...
 ping -n %interval% 127.0.0.1 > nul
 goto loop
